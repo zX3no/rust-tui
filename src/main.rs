eac3f2b9a16e77b21969f4d9461fc953b61fd748
zX3no <dr_xeno@hotmail.com.au>
2021-06-29 21:00:58 +0930

    task and header function

--- src/main.rs
+++ src/main.rs
@@ -2,49 +2,81 @@
 use std::io::{stdout, Write};
 
 #[allow(unused_imports)]
+use std::string;
+
+#[allow(unused_imports)]
 use crossterm::{
-    execute, event, terminal,
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor, SetAttribute},
-    ExecutableCommand, Result,
+    event, execute,
     style::Attribute,
+    style::{Color, Print, ResetColor, SetAttribute, SetBackgroundColor, SetForegroundColor},
+    terminal, ExecutableCommand, Result,
 };
 
-fn main() -> Result<()> {
+fn header() {
     execute!(
         stdout(),
         Print(" "),
         SetAttribute(Attribute::Underlined),
-        Print("Tasks:"), 
+        Print("Tasks:"),
         ResetColor,
         SetForegroundColor(Color::DarkGrey),
         Print(" [0/10]"),
         ResetColor
-    )?;
+    )
+    .ok();
+}
+
+fn task(id: i32, checked: bool, text: &str) {
     execute!(
         stdout(),
         Print("\n"),
         SetForegroundColor(Color::DarkGrey),
         Print("   "),
-        Print("1. "),
-        Print(" "),
-        SetForegroundColor(Color::Green),
-        Print("√"),
-        Print(" "),
-        ResetColor,
-        Print(" "),
-        Print("Do this?"),
-    )?;
-    execute!(
-        stdout(),
-        Print("\n"),
-        SetForegroundColor(Color::DarkGrey),
-        Print("   "),
-        Print("1. "),
-        SetForegroundColor(Color::DarkMagenta),
-        Print("[ ]"),
-        ResetColor,
-        Print(" "),
-        Print("Do this?"),
-    )?;
+        Print(id)
+    )
+    .ok();
+
+    if checked {
+        if id < 10 {
+            execute!(stdout(), Print(".    ")).ok();
+        } else if id < 100 {
+            execute!(stdout(), Print(".   ")).ok();
+        } else {
+            execute!(stdout(), Print(".  ")).ok();
+        }
+        execute!(
+            stdout(),
+            SetForegroundColor(Color::Green),
+            Print("√  "),
+            SetForegroundColor(Color::DarkGrey),
+            Print(text),
+        )
+        .ok();
+    } else {
+        if id < 10 {
+            execute!(stdout(), Print(".   ")).ok();
+        } else if id < 100 {
+            execute!(stdout(), Print(".  ")).ok();
+        } else {
+            execute!(stdout(), Print(". ")).ok();
+        }
+        execute!(
+            stdout(),
+            SetForegroundColor(Color::DarkMagenta),
+            Print("[ ]"),
+            ResetColor,
+            Print(" "),
+            Print(text)
+        )
+        .ok();
+    }
+}
+fn main() -> Result<()> {
+    header();
+    task(10, true, "AAAAAAA");
+    task(2, false, "Test");
+    task(15, false, "Make this very hard project");
+    task(999, false, "remember to do something");
+    task(9, true, "Takssks");
     Ok(())
 }
